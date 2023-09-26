16034a2d3188e1cf6cbdcd6b90fa6f99dd145bef
IvanA-18 <100191862+IvanA-19@users.noreply.github.com>
2023-09-26 12:44:14 +0300

    Added database and app News

---
+++ mwWebApp/news/models.py
@@ -0,0 +1,16 @@
+from django.db import models
+
+
+# Create your models here.
+class Articles(models.Model):
+    title = models.CharField('Name', max_length=50)
+    anons = models.CharField('Anons', max_length=250)
+    full_text = models.TextField('Article')
+    date = models.DateTimeField('Date of publishing')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = "Новость"
+        verbose_name_plural = "Новости"
